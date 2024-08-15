87f5f92f592560486a05cffa93fd9a9962c255bd
Shafqat Muneer <smuneer@2u.com>
2024-08-15 21:30:37 +0500

    feat: Crashlytics Integration with FullStory (#68)

--- OpenEdX/Managers/FullStoryAnalyticsService/FullStoryAnalyticsService.swift
+++ OpenEdX/Managers/FullStoryAnalyticsService/FullStoryAnalyticsService.swift
@@ -8,8 +8,16 @@
 import Foundation
 import Core
 import FullStory
+import FirebaseCrashlytics
 
-class FullStoryAnalyticsService: AnalyticsService {
+class FullStoryAnalyticsService: NSObject, AnalyticsService, FSDelegate {
+    
+    var firebaseEnabled: Bool
+    init(_ firebaseEnabled: Bool) {
+        self.firebaseEnabled = firebaseEnabled
+        super.init()
+        FS.delegate = self
+    }
     
     func identify(id: String, username: String?, email: String?) {
         FS.identify(id, userVars: ["displayName": id])
@@ -21,5 +29,14 @@ class FullStoryAnalyticsService: AnalyticsService {
     
     func logScreenEvent(_ event: Core.AnalyticsEvent, parameters: [String: Any]?) {
         FS.page(withName: event.rawValue, properties: parameters).start()
+    }
+
+    func fullstoryDidStartSession(_ sessionUrl: String) {
+        if firebaseEnabled {
+            Crashlytics.crashlytics().setCustomValue(
+                sessionUrl,
+                forKey: "fullstory_session_url"
+            )
+        }
     }
 }
